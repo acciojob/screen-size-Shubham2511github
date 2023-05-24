e01cef342848e64d1f339b4d4b540ed7c4b91c51
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-05-24 16:18:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function updateSizeinfo() {
+	const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+	const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+	const changeElement = document.querySelector("h1");
+	changeElement.textContent = `Width: ${width} and Height: ${height}`
+	
+}
+
+window.addEventListener("resize", updateSizeinfo);
+updateSizeinfo();
